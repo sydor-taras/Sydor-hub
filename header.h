0fe5bd112f310e05d34dc5637a553a8450be1f84
Sydor <sydor.taras.vol@gmail.com>
2012-11-03 08:45:46 +0300

    first commit git remote add origin git@github.com:sydor-taras/Sydor-hub.git git push -u origin master exit

---
+++ header.h
@@ -0,0 +1,17 @@
+/**
+* header.h
+* includes constants and declaration of a function
+* Copyright 2012 by Sydor Taras
+*/
+
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef unsigned long UL;							//my own name for unsigned long
+typedef unsigned short US;
+
+UL Exchange(UL a);									//reserving memory for Exchange()
+
+UL Enter(void);
+
+#endif 
