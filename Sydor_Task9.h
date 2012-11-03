7f25239c23d5d5526a12ad194c1623fda276f22d
sydor-taras <sydor.taras.vol@gmail.com>
2012-11-03 19:58:14 +0300

    first commit

---
+++ Sydor_Task9.h
@@ -0,0 +1,16 @@
+/**
+* Sydor_Task9.h
+* includes constants and declaration of a function
+* Copyright 2012 by Sydor Taras
+*/
+
+#ifndef SYDOR_TASK9_H
+#define SYDOR_TASK9_H
+
+typedef unsigned long UL;							//my own name for unsigned long
+
+UL f(UL a);											//this function makes all bits '0',except the younger '0'	
+
+UL Enter(void);										//this function reads a number from keyboard				
+
+#endif
